cc6cd001a54f528ee4657c9b36bf5c0a2e26322c
Carolyn <carito.ulfe@gmail.com>
2017-01-19 02:41:52 -0500

    Using Flight API - /airlines service

---
+++ tools/backServer.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import request from 'request'; // http calls
+
+const app =  express();
+const base_url = 'http://node.locomote.com/code-task/';
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/airlines',function(req,res){
+  request.get(base_url+'airlines', function(error, response, body){
+    if(!error && response.statusCode == 200){
+      res.send(JSON.parse(body));
+    }
+  });
+});
+
+app.listen(3001);
