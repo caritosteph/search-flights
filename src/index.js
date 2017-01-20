b4de91e689ffb7f303b287d45dbccf9785c4919b
Carolyn <carito.ulfe@gmail.com>
2017-01-19 23:59:50 -0500

    form flight search

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.min';
+import '../node_modules/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css';
+import '../node_modules/bootstrap-datepicker/dist/js/bootstrap-datepicker.min';
+import './styles/main.css';
+
+$('#travel-date input').datepicker({
+    clearBtn: true,
+    orientation: "bottom left",
+    keyboardNavigation: false,
+    forceParse: false
+});
