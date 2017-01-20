a36d824e40f2ea473fb462742c3c98bcbbf16d4f
Carolyn <carito.ulfe@gmail.com>
2017-01-20 01:46:38 -0500

    form submit

--- src/index.js
+++ src/index.js
@@ -5,8 +5,29 @@ import '../node_modules/bootstrap-datepicker/dist/js/bootstrap-datepicker.min';
 import './styles/main.css';
 
 $('#travel-date input').datepicker({
+    format: "yyyy/mm/dd",
     clearBtn: true,
     orientation: "bottom left",
     keyboardNavigation: false,
     forceParse: false
 });
+
+$('#search-flight').submit(function(e){
+  e.preventDefault();
+  let from = $('input[name=from]').val();
+  let to  = $('input[name=to]').val();
+  let date = $('input[name=date]').val();
+  let query = {from, to, date};
+  search_flight(query);
+});
+
+function search_flight(params){
+  let request = $.ajax({
+                  url: 'http://localhost:3002/search',
+                  method: 'GET',
+                  data: params
+                });
+  request.done(function(){
+    console.log("work");
+  });
+}
